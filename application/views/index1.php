cf6949c7fc34adf4f757d8c1fafe7a0a38d9c799
Keikishim2 <Keikishim2@gmail.com>
2021-07-28 14:46:22 +0800

    last

--- application/views/index1.php
+++ application/views/index1.php
@@ -16,61 +16,14 @@
 </head>
 <body>
 	<header>
-		<nav class="wrap">
-			<div class="container">
-				<a class="navbar-brand" href="#">PETSHELTER</a>
-				<aside class="nav-links">
-					<a href="#" class="active">Home</a>
-					<a href="#">Services</a>
-					<a href="#">Event</a>
-					<button type="button" class="btn btn-add-pet-to-shelter" data-toggle="modal" data-target="#addPet"><i class="fa fa-plus-circle" aria-hidden="true"></i>Add pet to shelter
-					</button>
-					<section><!--DOCU
-								Adding pet to the shelter
-
-								OWNER: Kei Kishimoto-->
-						<div class="modal fade" id="addPet" aria-labelledby="addPet" aria-hidden="true">
-							<div class="modal-dialog">
-								<div class="modal-content">
-									<div class="modal-header">
-										<h5 class="modal-title" id="addPet">Add Pet</h5>
-										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-											<span aria-hidden="true">&times;</span>
-										</button>
-									</div>
-									<form action="#" method="POST">
-										<div class="modal-body">
-											<div class="form-group">
-												<div class="col-md-3">
-													<label for="pet-name">Pet Name</label>
-												</div>
-												<div class="col-md-9">
-													<input type="text" class="form-control" id="pet-name" name="pet-name" aria-describedby="emailHelp" />
-												</div>
-											</div>
-											<div class="form-group">
-												<div class="col-md-3">
-													<label for="pet-name">Pet Type</label>
-												</div>
-												<div class="col-md-9">
-													<select class="form-control custom-select" id="pet-type" name="pet-type">
-														<option>Cat</option>
-														<option>Dog</option>
-														<option>Penguin</option>
-														<option>Duck</option>
-													</select>
-												</div>
-											</div>
-										</div>
-										<div class="modal-footer">
-											<button type="button" class="btn btn-add" data-dismiss="modal"><i class="fas fa-save"></i>Add Pet</button>
-										</div>
-									</form>
-								</div>
-							</div>
-						</div>
-					</section>
-				</aside>
+		<nav>
+			<a class="navbar-brand" href="#">PETSHELTER</a>
+			<div class="nav-links">
+				<a href="#" class="active">Home</a>
+				<a href="#">Services</a>
+				<a href="#">Event</a>
+				<button type="button" class="btn btn-add-pet-to-shelter" data-toggle="modal" data-target="#addPet"><i class="fa fa-plus-circle" aria-hidden="true"></i>Add pet to shelter
+				</button>
 			</div>
 		</nav>
 	</header>
@@ -82,104 +35,129 @@
 				--><p>Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit. Neque id lorem nisi.</p>
 			</div>
 			<h3>These pets are looking for a good home</h3>
-			</div>
 		</main>
 		<div class="pets-section">
-			<div class="container">
-				<div class="row">
-					<div class="pet-lists">
-						<section>
-							<menu class="all_pets" data-pet="Garfield">
-								<p>Garfield</p>
-								<p>Cat</p>
-								<div class="action">
-									<a href="#" role="button" class="detailsPet-open-modal" data-toggle="modal" data-target=".detailsPet"><i class="fas fa-clipboard-list"></i>Details</a>
-									<a href="#" role="button" data-toggle="modal" class="editPet-open-modal" data-target=".editPet"><i class="fas fa-edit"></i> Edit</a>
-								</div>
-							</menu>
-						</section>
-					</div>
-				</div>
-				<section><!--DOCU
-								Viewing pet details
-
-								OWNER: Kei Kishimoto-->
-					<div class="row">
-						<div class="modal fade detailsPet" id="detailsPet" tabindex="-1" aria-labelledby="detailsPet" aria-hidden="true">
-							<div class="modal-dialog">
-								<div class="modal-content">
-									<div class="modal-header">
-										<h5 class="modal-title" id="detailsPet">Details About: <span class="pet-name">Garfield</span></h5>
-										<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
-										</button>
-									</div>
-									<form class="form-info">
-										<div class="modal-body">
-											<div class="form-group">
-												<div class="col-md-3">
-													<label for="pet-type">Pet Type</label>
-												</div>
-												<div class="col-md-9">
-													<p id="pet-type" class="pet-type">Cat</p>
-												</div>
-											</div>
-										</div>
-										<div class="modal-footer">
-											<p><span class="likes">0</span> Likes</p>
-											<div class="button-group">
-												<button type="button" id="btn-like" class="btn btn-like-pet" data-dismiss="modal"><i class="fas fa-heart"></i>Like <span 	class="pet-name"></span>
-												</button>
-												<button type="button" class="btn btn-adopt-pet" data-dismiss="modal"><i class="fa fa-home" aria-hidden="true"></i>Adopt <span class="pet-name"></span>
-												</button>
-											</div>
-										</div>
-									</form>
-								</div>
+			<div class="row">
+				<div class="pet-lists">
+					<section>
+						<menu class="all_pets" data-pet="Garfield">
+							<p>Garfield</p>
+							<p id="type">Cat</p>
+							<div class="action">
+								<a href="#" role="button" class="detailsPet-open-modal" data-toggle="modal" data-target=".detailsPet"><i class="fas fa-clipboard-list"></i>Details</a>
+								<a href="#" role="button" data-toggle="modal" class="editPet-open-modal" data-target=".editPet"><i class="fas fa-edit"></i> Edit</a>
 							</div>
-						</div>
-					</div>
-				</section>
-					<section><!--DOCU
-								Pet editing in modal form
-
-								OWNER: Kei Kishimoto-->
-						<div class="modal fade editPet" id="editPet" tabindex="-1" aria-labelledby="editPet" aria-hidden="true">
-							<div class="modal-dialog">
-								<div class="modal-content">
-									<div class="modal-header">
-										<h5 class="modal-title" id="addPet">Edit Details: <span class="pet-name">Garfield</span></h5>
-										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-											<span aria-hidden="true">&times;</span>
-										</button>
-									</div>
-									<form class="form-info">
-										<div class="modal-body">
-											<div class="form-group">
-												<div class="col-md-3">
-													<label for="pet-name">Pet Type</label>
-												</div>
-												<div class="col-md-9">
-													<select class="form-control custom-select pet-type" id="pet-type-edit" name="pet-type">
-														<option>Cat</option>
-														<option>Dog</option>
-														<option>Penguin</option>
-														<option>Duck</option>
-													</select>
-												</div>
-											</div>
-										</div>
-										<div class="modal-footer">
-											<button type="button" class="btn btn-update" data-dismiss="modal"><i class="fas fa-save"></i>Save Changes</button>
-										</div>
-									</form>
-								</div>
-							</div>
-						</div>
+						</menu>
 					</section>
 				</div>
 			</div>
 		</div>
 	</section>
-	<footer>@2020<span class="v88"> V88Studios</span>. All Rights Reserved</footer>
+	<footer>@2020<span class="v88"> V88Studios</span>. All Rights Reserved.<br>Redirect<a href="https://keikishim2.github.io/learning_timeline/"> HERE</footer>
+<!-- DOCU: Add Pet-->
+	<div class="modal fade" id="addPet" aria-labelledby="addPet" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="addPet">Add Pet</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<form action="#" method="POST">
+					<div class="modal-body">
+						<div class="form-group">
+							<div class="col-md-3">
+								<label for="pet-name">Pet Name</label>
+							</div>
+							<div class="col-md-9">
+								<input type="text" class="form-control" id="pet-name" name="pet-name" aria-describedby="emailHelp" />
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-md-3">
+								<label for="pet-name">Pet Type</label>
+							</div>
+							<div class="col-md-9">
+								<select class="form-control custom-select" id="pet-type" name="pet-type">
+									<option>Cat</option>
+									<option>Dog</option>
+									<option>Penguin</option>
+									<option>Duck</option>
+								</select>
+							</div>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-add" data-dismiss="modal"><i class="fas fa-save"></i>Add Pet</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+<!-- DOCU: Pet Details-->
+	<div class="row">
+		<div class="modal fade detailsPet" id="detailsPet" tabindex="-1" aria-labelledby="detailsPet" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="detailsPet">Details About: <span class="pet-name">Garfield</span></h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					</div>
+					<form class="form-info">
+						<div class="modal-body">
+							<div class="form-group">
+								<div class="col-md-3">
+									<label for="pet-type">Pet Type</label>
+								</div>
+								<div class="col-md-9">
+									<p id="pet-type" class="pet-type">Cat</p>
+								</div>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<p><span class="likes">0</span> Likes</p>
+							<div class="button-group">
+								<button type="button" id="btn-like" class="btn btn-like-pet" data-dismiss="modal"><i class="fas fa-heart"></i>Like <span 	class="pet-name"></span></button>
+								<button type="button" class="btn btn-adopt-pet" data-dismiss="modal"><i class="fa fa-home" aria-hidden="true"></i>Adopt <span class="pet-name"></span></button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- DOCU: Edit Pet-->
+	<div class="modal fade editPet" id="editPet" tabindex="-1" aria-labelledby="editPet" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="addPet">Edit Details: <span class="pet-name">Garfield</span></h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span></button>
+				</div>
+				<form class="form-info">
+					<div class="modal-body">
+						<div class="form-group">
+							<div class="col-md-3">
+								<label for="pet-name">Pet Type</label>
+							</div>
+							<div class="col-md-9">
+								<select class="form-control custom-select pet-type" id="pet-type-edit" name="pet-type">
+									<option>Cat</option>
+									<option>Dog</option>
+									<option>Penguin</option>
+									<option>Duck</option>
+								</select>
+							</div>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-update" data-dismiss="modal"><i class="fas fa-save"></i>Save Changes</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,22 @@
 /* DOCU:
-    Using JSON to fetch objects in an array
-        - id,name,type
-
-    OWNER: Kei Kishimoto
+    Using JSON to fetch objects an array
+    @Author: Kei Kishimoto
 */
 $(document).ready(function () {
 	let all_pets = [
-        { id: 1, name: "Garfield", type: "Cat"},
-		{ id: 2, name: "Doraemon", type: "Cat"},
+        {id: 1, name: "Garfield", type: "Cat"},
+		{id: 2, name: "Doraemon", type: "Cat"},
 		{id: 3, name: "Snoopy", type: "Dog"},
 		{id: 4,name: "Daffy", type: "Duck"},
 		{id: 5, name: "Pen Pen", type: "Penguin"},
 	];
-/* DOCU:
-    Using function to insert details using for loop
 
-    OWNER: Kei Kishimoto
-*/
 	function pets() {
 		let pets = ``;
 		for (let i = 0; i < all_pets.length; i++) {
-			pets += `<menu class="all_pets" data-pet="${all_pets[i].name}">`;
+			pets += `<menu class="all_pets" data-pet="${all_pets[i].id}">`;
 			pets += `	<p>${all_pets[i].name}</p>`;
-			pets += `	<p>${all_pets[i].type}</p>`;
+			pets += `	<p id="type">${all_pets[i].type}</p>`;
 			pets += `	<div class="action">`;
 			pets += `		<a href="#" role="button" class="detailsPet-open-modal" data-toggle="modal" data-target=".detailsPet"><i class="fas fa-clipboard-list"></i> Details</a>`;
 			pets += `		<a href="#" role="button" data-toggle="modal" class="editPet-open-modal" data-target=".editPet"><i class="fas fa-edit"></i> Edit</a>`;
@@ -31,55 +25,58 @@ $(document).ready(function () {
 		}
 		$(".pet-lists").html(pets);
 	}
+	$("body")
+		.on("click", ".btn-add", addPet)
+		.on("click", ".detailsPet-open-modal", showPetDetails)
+		.on("click", ".editPet-open-modal", editPet)
+		.on("click", ".btn-update", updatePet);
+		
 /* DOCU:
-    Adding pet on the list with validations
-        
-    OWNER: Kei Kishimoto
+    Add New Pet
+	TRIGGERED BY: .btn-add
+    @Author: Kei Kishimoto
 */
-	$(".btn-add")
-    .click(function () {
+	function addPet(){
 		if ($("#pet-name").val() == "") {
 			alert("Pet name cannot be blank!");
 			return false;
 		}
-		let new_pet = {
-			id: all_pets.length + 1,
-			name: $("#pet-name").val(),
-			type: $("#pet-type").val(),
-		};
-		all_pets.unshift(new_pet);
-		pets();
-		$(".added-pet-name").html($("#pet-name").val());
-		$("#pet-name").val("");
-		$("#pet-type").val($("#pet-type option:first").val());
-	});
+	let new_pet = {
+		id: all_pets.length + 1,
+		name: $("#pet-name").val(),
+		type: $("#pet-type").val(),
+	};
+	all_pets.unshift(new_pet);
+	pets();
+	$(".added-pet-name").html($("#pet-name").val());
+	$("#pet-name").val("");
+	$("#pet-type").val($("#pet-type option:first").val());
+	}
 /* DOCU:
-    Vieweing pet details
-        
-    OWNER: Kei Kishimoto
+    Show Pet Details
+	TRIGGERED BY: .detailsPet-open-modal
+    @Author: Kei Kishimoto
 */
-	$(document)
-    .on("click", ".detailsPet-open-modal", function (e) {
+	function showPetDetails(e) {
 		e.preventDefault();
 		$(".detailsPet .modal-dialog .modal-content .modal-header .modal-title .pet-name").html($(this).parent().siblings()[0].innerHTML);
 		$(".detailsPet .modal-dialog .modal-content .modal-body .pet-type").html($(this).parent().siblings()[1].innerHTML);
 		$(".detailsPet .modal-dialog .modal-content .modal-footer .pet-name").html($(this).parent().siblings()[0].innerHTML);
-	});
-	$(document)
-    .on("click", ".editPet-open-modal", function (e) {
-		e.preventDefault();
-		$(".editPet .modal-dialog .modal-content .modal-header .modal-title .pet-name").html($(this).parent().siblings()[0].innerHTML);
-		$(`.editPet .modal-dialog .modal-content .modal-body .pet-type option:contains("${$(this).parent().siblings()[1].innerHTML}")`).prop(
-			"selected",
-			true
-		);
-	});
+	}
 /* DOCU:
-        Editing pet details using for loop
-        
-        OWNER: Kei Kishimoto
+    Edit Pet Details
+	TRIGGERED BY: .editPet-open-modal
+    @Author: Kei Kishimoto
 */
-	$(document).on("click", ".btn-update", function () {
+	function editPet(e){
+		e.preventDefault();
+			$(".editPet .modal-dialog .modal-content .modal-header .modal-title .pet-name").html($(this).parent().siblings()[0].innerHTML);
+			$(`.editPet .modal-dialog .modal-content .modal-body .pet-type option:contains("${$(this).parent().siblings()[1].innerHTML}")`).prop(
+				"selected",
+				true
+		);
+	}
+	function updatePet() {
 		let pet_to_edit = $(".editPet .modal-dialog .modal-content .modal-header .modal-title .pet-name").html();
 		for (let i = 0; i < all_pets.length; i++) {
 			if (pet_to_edit == all_pets[i].name) {
@@ -87,6 +84,6 @@ $(document).ready(function () {
 			}
 		}
 		pets();
-	});
+	};
 	pets();
 });
